734c8a0b009970f313ac07399ec572ca3d1a5303
shilpisoni72 <78276461+shilpisoni72@users.noreply.github.com>
2021-11-09 04:21:15 -0800

    Revert my changes from reservation controller
    This reverts commit d033247938e47c37e605488b9a84d5389023a7aa.

--- src/main/java/edu/sjsu/cmpe275/Controller/ReservationController.java
+++ src/main/java/edu/sjsu/cmpe275/Controller/ReservationController.java
@@ -1,17 +1,18 @@
 package edu.sjsu.cmpe275.Controller;
 
+import edu.sjsu.cmpe275.Helper.Error.Response;
 import edu.sjsu.cmpe275.Model.Flight;
 import edu.sjsu.cmpe275.Model.Passenger;
 import edu.sjsu.cmpe275.Model.Reservation;
 import edu.sjsu.cmpe275.Repository.FlightRepository;
 import edu.sjsu.cmpe275.Repository.PassengerRepository;
 import edu.sjsu.cmpe275.Repository.ReservationRepository;
-import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+//import com.fasterxml.jackson.xml.XmlMapper;
+
 
 import java.util.*;
 
@@ -30,127 +31,106 @@ public class ReservationController {
     PassengerRepository passengerRepository;
 
     @GetMapping("/")
-    public ResponseEntity<List<Reservation>> getAllReservations() {
+    public ResponseEntity<Object> getAllReservations() {
         try {
 
-            List<Reservation> reservations = new ArrayList<Reservation>();
+            return new ResponseEntity<Object>(new edu.sjsu.cmpe275.Helper.Success.Response("404", "Hanish"), HttpStatus.OK);
 
-            List<Long> ids = new ArrayList<Long>();
-            ids.add(5L);
-            ids.add(2L);
-
-            reservationRepository.findAllById(ids).forEach(reservations::add);
-            reservations.sort(Comparator.comparing(Reservation::getReservationNumber));
-            Collections.reverse(reservations);
-
-            if (reservations.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            return new ResponseEntity<>(reservations, HttpStatus.OK);
+//            List<Reservation> reservations = new ArrayList<Reservation>();
+//
+//            List<Long> ids = new ArrayList<Long>();
+//            ids.add(5L);
+//            ids.add(2L);
+//
+//            reservationRepository.findAllById(ids).forEach(reservations::add);
+//            reservations.sort(Comparator.comparing(Reservation::getReservationNumber));
+//            Collections.reverse(reservations);
+//
+//            if (reservations.isEmpty()) {
+//                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//            }
+//            return new ResponseEntity<Object>(reservations, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/{number}")
-    public ResponseEntity<Reservation> getReservation(@PathVariable("number") Long id) {
+    public ResponseEntity<Object> getReservation(@PathVariable("number") Long id) {
         try {
-
             Optional<Reservation> reservationData = reservationRepository.findById(id);
-
-            if (reservationData.isPresent()) {
+            if (reservationData.isPresent())
                 return new ResponseEntity<>(reservationData.get(), HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-//            return new ResponseEntity<>(reservations, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            else
+                return new ResponseEntity<Object>(new Response("404", "Passenger not found"), HttpStatus.NOT_FOUND);
+        } catch (Exception exception) {
+            return new ResponseEntity<Object>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-	
+
     @DeleteMapping("/{number}")
     public ResponseEntity<?> deleteReservation(@PathVariable Long number) throws Exception {
-   	 //System.out.print("inside delete");
-   	HashMap<String, Object> map = new HashMap<>();
-   	HashMap<String, Object> mapnew = new HashMap<>();
-   	  Optional<Reservation> reservation =
-   			   reservationRepository
-   	           .findById(number);  		
-   		if(!reservation.isPresent())
-   		{   			
-   	   	    mapnew.clear();
-   	   	    map.clear();
-   		    map.put("code", "404");
-   		    map.put("msg", "reservation with number "+number+" does not exist");
-		    mapnew.put("Bad Request", map);
-   			return new ResponseEntity<>(mapnew, HttpStatus.NOT_FOUND);
+        //System.out.print("inside delete");
+        HashMap<String, Object> map = new HashMap<>();
+        HashMap<String, Object> mapnew = new HashMap<>();
+        Optional<Reservation> reservation =
+                reservationRepository
+                        .findById(number);
+        if (!reservation.isPresent()) {
+            mapnew.clear();
+            map.clear();
+            map.put("code", "404");
+            map.put("msg", "reservation with number " + number + " does not exist");
+            mapnew.put("Bad Request", map);
+            return new ResponseEntity<>(mapnew, HttpStatus.NOT_FOUND);
 
-   		}
-   		else
-   		{   		
-   			Optional<Reservation> reservationData = reservationRepository.findById(number);
-   			if(reservationData.isPresent())
-   			{
-   				Reservation currentReservation = reservationData.get();
-   				List<Flight> currentReservationFlights = currentReservation.getFlights();
-   				for(Flight fl : currentReservationFlights) {
-   					fl.setSeatsLeft(fl.getSeatsLeft()+1);
+        } else {
+            Optional<Reservation> reservationData = reservationRepository.findById(number);
+            if (reservationData.isPresent()) {
+                Reservation currentReservation = reservationData.get();
+                List<Flight> currentReservationFlights = currentReservation.getFlights();
+                for (Flight fl : currentReservationFlights) {
+                    fl.setSeatsLeft(fl.getSeatsLeft() + 1);
 
-	                }
-   			}
-   			reservationRepository.deleteById(number);
+                }
+            }
+            reservationRepository.deleteById(number);
 
-   			return new ResponseEntity<>(reservation, HttpStatus.OK);
-   			//return new ResponseEntity<>((generateErrorMessage("Response", "200", "Passenger with id " + number + " is deleted successfully")),HttpStatus.OK);
-   		}
-   		
-   	
-   }
+            return new ResponseEntity<>(reservation, HttpStatus.OK);
+            //return new ResponseEntity<>((generateErrorMessage("Response", "200", "Passenger with id " + number + " is deleted successfully")),HttpStatus.OK);
+        }
 
-    @PostMapping("")
-    public ResponseEntity<Reservation> makeReservation(@RequestParam("passengerId") Long passengerId, @RequestParam("flightNumbers") List<Long> flightNumbers) {
 
+    }
+
+    @PostMapping()
+    public ResponseEntity<Object> makeReservation(@RequestParam("passengerId") Long passengerId, @RequestParam("flightNumbers") List<Long> flightNumbers) {
         try {
-
             List<Flight> flights = new ArrayList<Flight>();
             flightRepository.findAllById(flightNumbers).forEach(flights::add);
-
-            if (!checkSeatsLeft(flights)) {
-//                return error
-                return null;
-            }
-
-            if (!checkOverlap(flights)) {
-                //return error
-                return null;
-            }
+            if (!checkSeatsLeft(flights))
+                return new ResponseEntity<Object>(new Response("400", "No seats left on the flight"), HttpStatus.BAD_REQUEST);
+            if (!checkOverlap(flights))
+                return new ResponseEntity<Object>(new Response("400", "The flights overlap with each other"), HttpStatus.BAD_REQUEST);
             Optional<Passenger> PassengerData = passengerRepository.findById(passengerId);
             Passenger passenger = PassengerData.get();
-            if (PassengerData.isEmpty()) {
-                //return error
-                return null;
-            }
-
-            if (!checkReservationsOverlap(passenger, flights)) {
-//                return error
-                return null;
-            }
-
+            if (PassengerData.isEmpty())
+                return new ResponseEntity<Object>(new Response("404", "The passenger does not exist"), HttpStatus.NOT_FOUND);
+            if (!checkReservationsOverlap(passenger, flights))
+                return new ResponseEntity<Object>(new Response("400", "The flights overlap with other reservations"), HttpStatus.BAD_REQUEST);
             Reservation reservation = createReservation(passenger, flights);
-            
+            if(reservation==null)
+                return new ResponseEntity<Object>(new Response("500", "Some error occurred"), HttpStatus.INTERNAL_SERVER_ERROR);
             for (Flight flight :
                     flights) {
                 passenger.getFlights().add(flight);
                 flight.setSeatsLeft(flight.getSeatsLeft() - 1);
                 flightRepository.save(flight);
             }
-
             passengerRepository.save(passenger);
-            return new ResponseEntity<>(reservationRepository.save(reservation), HttpStatus.OK);
-
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<Object>(reservationRepository.save(reservation), HttpStatus.OK);
+        } catch (Exception exception) {
+            return new ResponseEntity<Object>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -228,5 +208,4 @@ public class ReservationController {
     }
 
 }
-
 
