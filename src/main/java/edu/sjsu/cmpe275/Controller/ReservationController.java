f55789163708a5ce90ba682f2dc7f7734c389550
shilpisoni72 <78276461+shilpisoni72@users.noreply.github.com>
2021-11-09 03:15:40 -0800

    update reservation api

--- src/main/java/edu/sjsu/cmpe275/Controller/ReservationController.java
+++ src/main/java/edu/sjsu/cmpe275/Controller/ReservationController.java
@@ -107,6 +107,294 @@ public class ReservationController {
    		
    	
    }
+    
+    
+    
+//    @PostMapping("/{number}")
+//	public ResponseEntity<?> updateReservaton(
+//			@PathVariable int number,
+//			@RequestParam(value="flightsAdded", required=false) String flightsAdded, 
+//			@RequestParam(value="flightsRemoved", required=false) String flightsRemoved){
+//    	
+//    
+//    	
+//        List<Flight> flightlist = new ArrayList<Flight>();
+//    	//Set<Flight> flightlist = new HashSet<Flight>();
+//        List<Flight> flightlisthere = new ArrayList<Flight>();
+//        flightlist.clear();
+//     
+//        Optional<Reservation> reservationData = reservationRepository.findById((long)number); //get reservation details
+//        Reservation currentReservation = reservationData.get();
+//		List<Flight> currentReservationFlights = currentReservation.getFlights();
+//		for(Flight fl : currentReservationFlights) {
+//			if(fl!=null) {
+//			flightlist.add(fl);
+//			}
+//				System.out.println("flight list for the reservation" + fl.getFlightNumber()); //flight list for particular reservation
+//
+//            }
+//
+//		
+//	  	HashMap<String, Object> map = new HashMap<>();
+//	   	HashMap<String, Object> mapnew = new HashMap<>();
+//		List<Flight> flightAddedObects=null;
+//		List<Flight> flightRemovedObects=null;
+//
+//		if(flightsAdded!=null){
+//			//add the flights seperated by comma to the list
+//			String[] flightsAddedList=flightsAdded.split(",");
+//			List<Long> numberflightsAddedList=new ArrayList<Long>();
+//			List<Long> FlightsAddedPresentinFlightTable=new ArrayList<Long>(); // consists of all the fights which are present in the flight table and hence can update the reservation.
+//
+//			
+//			for(String nu : flightsAddedList) {
+//				numberflightsAddedList.add(Long.parseLong(nu)); 
+//				System.out.println("added flight params" + numberflightsAddedList);
+//				}
+//			
+//			
+//
+//			System.out.print("numberflightsAddedList" + numberflightsAddedList.size());
+//			List<Flight> flights=new ArrayList<Flight>();
+//			for(Long s: numberflightsAddedList)
+//			{
+//				Optional<Flight> flightnew =flightRepository.findById(s);
+//				if(flightnew.isEmpty())
+//				{
+//					    mapnew.clear();
+//			   	   	    map.clear();
+//			   		    map.put("code", "404");
+//			   		    map.put("msg", "Flight number "+s+" does not exist and hence cannot be added");
+//					    mapnew.put("Bad Request", map);
+//			   			return new ResponseEntity<>(mapnew, HttpStatus.NOT_FOUND);
+//				}
+//				else
+//				{
+//						FlightsAddedPresentinFlightTable.add((s));
+//				
+//				}
+//			}
+//			
+//			 Flight flight;
+//			 System.out.print("FlightsAddedPresentinFlightTable" + FlightsAddedPresentinFlightTable);
+//			for(Long fligtiter:FlightsAddedPresentinFlightTable)
+//			{
+//				Optional<Flight> flightData = flightRepository.findById(fligtiter);
+//				  flight = flightData.get();
+//				  if(flight!=null) {
+//					  flightlist.add(flight);
+//				  }
+//				  
+//				  //boolean isoverlapping = false;
+//				 
+//				 boolean isoverlapping = checkOverlap(new ArrayList<Flight> (flightlist));
+//					System.out.print("\n isoverlapping" + isoverlapping);
+////				 if(!isoverlapping)
+////				 {
+////					 currentReservation.getFlights().add(flight);
+////				}
+//				
+//				 //reservationRepository.save(currentReservation);				 
+//			}
+//			
+//			for(Flight flt:flightlist)
+//			{
+//				//boolean overlap = false;
+//				boolean overlap = checkOverlap((flightlist));
+//				if(!overlap)
+//				{
+//					currentReservation.getFlights().add(flt);
+//				}
+//				else
+//				{
+//					System.out.print("\n \n \n here false");
+//				}
+//				
+//				reservationRepository.save(currentReservation);
+//			}
+//			 
+//
+//
+//			 System.out.println(" inside for FlightsAddedPresentinFlightTable : " + flightlist.size());
+//
+//		}
+//		else
+//		{
+//			return new ResponseEntity<>(mapnew, HttpStatus.NOT_FOUND);
+//		}
+//		
+//		
+//		
+//		
+//	
+//		
+//
+//		
+//		return new ResponseEntity<>(currentReservation, HttpStatus.NOT_FOUND);
+//	}
+//
+
+    
+    
+    @PostMapping("/{number}")
+   	public ResponseEntity<?> updateReservaton(
+   			@PathVariable int number,
+   			@RequestParam(value="flightsAdded", required=false) String flightsAdded, 
+   			@RequestParam(value="flightsRemoved", required=false) String flightsRemoved){
+       	 try {
+       	  // List<Flight> flightlist = new ArrayList<Flight>();
+              Set<Flight> flightlist = new HashSet<Flight>();
+              Set<Flight> flights_new =new HashSet<Flight>();
+              Set<Flight> flights_removed =new HashSet<Flight>();
+			   Optional<Reservation> reservationData = reservationRepository.findById((long) number); // get																					// details
+				Reservation currentReservation = reservationData.get();
+				List<Flight> currentReservationFlights = currentReservation.getFlights();
+				for (Flight fl : currentReservationFlights) {
+					if (fl != null) {
+						flightlist.add(fl);
+					}
+					System.out.println("flight list for the reservation" + fl.getFlightNumber()); // flight list for
+				}
+				
+				List<Flight> flightAddedObects=null;
+				List<Flight> flightRemovedObects=null;
+		
+				if(flightsAdded!=null){
+					//add the flights seperated by comma to the list
+					String[] flightsAddedList=flightsAdded.split(",");
+					List<Long> numberflightsAddedList=new ArrayList<Long>();
+					
+					for(String nu : flightsAddedList) {
+						numberflightsAddedList.add(Long.parseLong(nu)); 
+						System.out.println("added flight params" + numberflightsAddedList);
+						}
+					
+					
+		
+					System.out.print("numberflightsAddedList" + numberflightsAddedList.size());
+					
+					for(Long s: numberflightsAddedList)
+					{
+						Optional<Flight> flightnew =flightRepository.findById(s);
+						if(!flightnew.isEmpty())
+						{
+							flights_new.add(flightnew.get());
+						}
+							    
+					}
+				}
+				
+				Set<Flight> nonOverlap_Flight = new HashSet<Flight>();
+					
+					for(Flight flight_new : flights_new) {
+						boolean isOverLap = false;
+						for(Flight flight: flightlist) {
+							if(flight.getFlightNumber() == flight_new.getFlightNumber() ) {
+								System.out.println("Already contain same flight" + flight.getFlightNumber() );
+								isOverLap = true;
+								return createBadRequest("Already contain same flight " + flight.getFlightNumber() );
+							}
+							if(flight.getDepartureTime().after(flight_new.getDepartureTime()) && flight.getArrivalTime().before(flight_new.getDepartureTime())) {
+								System.out.println("Time conflict happens with flight: " + flight.getFlightNumber());
+								isOverLap = true;
+								return createBadRequest("Time conflict happens with flight: " + flight.getFlightNumber() + " for flight number: " + flight_new.getFlightNumber() );
+							}
+							
+						}
+						if(!isOverLap) {
+							currentReservation.getFlights().add(flight_new);
+							nonOverlap_Flight.add(flight_new);
+						}
+					}
+					
+					
+					if(flightsRemoved!=null){
+						//add the flights seperated by comma to the list
+						String[] flightsRemovedList=flightsRemoved.split(",");
+						List<Long> numberflightsRemovedList=new ArrayList<Long>();
+						
+						for(String nu : flightsRemovedList) {
+							numberflightsRemovedList.add(Long.parseLong(nu)); 
+							System.out.println("added flight params" + numberflightsRemovedList);
+							}
+						
+						
+			
+						System.out.print("numberflightsAddedList" + numberflightsRemovedList.size());
+						
+						for(Long s: numberflightsRemovedList)
+						{
+							Optional<Flight> flightnew =flightRepository.findById(s);
+							if(!flightnew.isEmpty())
+							{
+								flights_removed.add(flightnew.get());
+							}
+								    
+						}
+					}
+					
+					for(Flight flights_remove : flights_removed) {
+						if(!nonOverlap_Flight.contains(flights_remove)) // to handle non-overlap scenario
+						{
+							currentReservation.getFlights().removeIf(flight -> flight.getFlightNumber() == flights_remove.getFlightNumber());
+						}
+						else
+						{
+							return createBadRequest("Trying to remove flight" + flights_remove.getFlightNumber() +" which was just added" );
+						}
+						
+					} 
+					updateReservation(currentReservation);
+					reservationRepository.save(currentReservation);
+					return new ResponseEntity<>(currentReservation, HttpStatus.OK);
+					
+					
+				
+				
+       	 } catch (Exception e) {
+       		 
+       	 }
+       	 
+     	return createBadRequest("Unknown Error");
+       	
+       	
+   	}
+
+
+
+    void updateReservation(Reservation reservation) {
+    	// update price 
+    	List<Flight> flights = reservation.getFlights();
+    	int price = 0;
+    	for(Flight flight : flights) {
+    		price += flight.getPrice();
+    	}
+    	reservation.setPrice(price);
+    	// update origin and destination
+    	
+    	
+    	// sort by departure date 
+    	flights.sort(Comparator.comparing(Flight::getDepartureTime));
+    	String origin = flights.get(0).getOrigin();
+    	String destination = flights.get(flights.size() - 1).getDestination();
+    	
+    	reservation.setOrigin(origin);
+    	reservation.setDestination(destination);
+    	
+    }
+    
+    ResponseEntity<?> createBadRequest(String msg)
+    {
+    	HashMap<String, Object> map = new HashMap<>();
+	   	HashMap<String, Object> mapnew = new HashMap<>();
+       	mapnew.clear();
+	   	map.clear();
+		map.put("code", "404");
+		map.put("msg", msg);
+	    mapnew.put("Bad Request", map);
+	    return new ResponseEntity<>(mapnew, HttpStatus.NOT_FOUND);
+    }
+   
 
     @PostMapping("")
     public ResponseEntity<Reservation> makeReservation(@RequestParam("passengerId") Long passengerId, @RequestParam("flightNumbers") List<Long> flightNumbers) {
@@ -115,7 +403,7 @@ public class ReservationController {
 
             List<Flight> flights = new ArrayList<Flight>();
             flightRepository.findAllById(flightNumbers).forEach(flights::add);
-
+ 
             if (!checkSeatsLeft(flights)) {
 //                return error
                 return null;
@@ -154,6 +442,28 @@ public class ReservationController {
         }
     }
 
+    
+    
+//    public static boolean checkOverlappingReservationforFlights(Set<Flight> flights) {
+//        try {
+//            Date arrivalTime = new Date();
+//            arrivalTime.setTime(0);
+//            Date departureTime = new Date();
+//            flights.sort(Comparator.comparing(Flight::getDepartureTime));
+//            for (Flight flight :
+//                    flights) {
+//                departureTime = flight.getDepartureTime();
+//
+//                if (arrivalTime.after(departureTime))
+//                    return false;
+//
+//                arrivalTime = flight.getArrivalTime();
+//            }
+//        } catch (Exception exception) {
+//            return false;
+//        }
+//        return true;
+//    }
     //checks for the flights passed in the parameter
     public static boolean checkOverlap(List<Flight> flights) {
         try {
